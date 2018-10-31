8267e4a570c30abb9c15a4fd55a5adfa9e1f879f
jessica-dbmachado <44639567+jessica-dbmachado@users.noreply.github.com>
2018-10-31 13:50:27 -0300

    Added model + repository
    Folder Model
    	-Class Candidate
    Folder Repository
    	-Class CandidateRepository

---
+++ src/main/java/br/edu/ulbra/election/candidate/model/Candidate.java
@@ -0,0 +1,65 @@
+package br.edu.ulbra.election.candidate.model;
+
+import javax.persistence.*;
+//string name  /  Long partyId /  Long numberElection / Long electionId 
+@Entity
+public class Voter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long partyId;
+
+    @Column(nullable = false)
+    private Long numberElection;
+	
+	@Column(nullable = false)
+    private Long electionId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+	
+	    public Long getPartyId() {
+        return partyId;
+    }
+
+    public void setPartyId(Long partyId) {
+        this.partyId = partyId;
+    }
+	
+	public Long getNumberElection() {
+        return numberElection;
+    }
+
+    public void setNumberElection(Long numberElection) {
+        this.numberElection = numberElection;
+    }
+	
+	public Long getElectionId() {
+        return electionId;
+    }
+
+    public void setElectionId(Long electionId) {
+        this.electionId = electionId;
+    }
+
+
+}
---
+++ src/main/java/br/edu/ulbra/election/candidate/repository/CandidateRepository.java
@@ -0,0 +1,7 @@
+package br.edu.ulbra.election.candidate.repository;
+
+import br.edu.ulbra.election.candidate.model.Candidate;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CandidateRepository extends CrudRepository<Candidate, Long> {
+}
