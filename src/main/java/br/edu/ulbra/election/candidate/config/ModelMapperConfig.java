00a3c1c4a745da87749ccb79d074b3563980f4c0
JeniferAbdo <JeniferAbdo@users.noreply.github.com>
2018-11-05 21:16:50 -0200

    build.gradle for modelmapper

--- src/main/java/br/edu/ulbra/election/candidate/config/ModelMapperConfig.java
+++ src/main/java/br/edu/ulbra/election/candidate/config/ModelMapperConfig.java
@@ -1,4 +1,4 @@
-package br.edu.ulbra.election.voter.config;
+package br.edu.ulbra.election.candidate.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
 
-    @Bean
+	@Bean
     public ModelMapper modelMapper() {
+    	ModelMapper modelMapper = new ModelMapper();
+    	modelMapper.getConfiguration().setAmbiguityIgnored(true);
         return new ModelMapper();
     }
 
